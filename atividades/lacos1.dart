9610552369a95a30a159ab6e3e36dab4f550921b
ErickSuirenAki <127347581+ErickSuirenAki@users.noreply.github.com>
2023-11-30 22:37:42 -0400

    aulas editadas

--- atividades/lacos1.dart
+++ atividades/lacos1.dart
@@ -18,6 +18,11 @@ void main() {
 
     for (int i = numero; i > 0; i--) {
       fatorial *= i;
+      /*fatorial *= i; está multiplicando a variável fatorial pelo valor de i e atribuindo o
+      resultado de volta a fatorial. Isso é equivalente a escrever fatorial = fatorial * i;. 
+      Com isso você multiplica o número que o usuário digitou por todos os numeros antecessores
+      dele até o 1. Exemplo: fatorial de 5 (ou, !5) = 5x4x3x2x1.
+      */
     }
     print("o fatorial do número que você digitou inicalmente é: $fatorial");
   }
