d9796880723ef9ad27da7b28a7a9da3dae5cab78
ErickSuirenAki <127347581+ErickSuirenAki@users.noreply.github.com>
2023-11-30 22:46:16 -0400

    aulas editadas

--- atividades/lacos2.dart
+++ atividades/lacos2.dart
@@ -8,9 +8,18 @@ void main() {
     lista.add(numero);
   }
   double media = lista.reduce((a, b) => a + b) / lista.length;
+  //o comando reduce serve para somar todos os valores de uma lista e juntar eles em um unico valor
+  /*
+  lista.reduce((a, b) => a + b) retorna a soma de todos os elementos na lista. Neste contexto,
+  está sendo utilizado para calcular a soma, que é posteriormente usada para calcular a média.
+
+  >>>>>NÂO LEMBRO EXATAMENTE O QUE SIGNIFICA<<<<<<<
+*/
   print("A media desse números inteiros que você digitou é $media");
 
   List<int> acima = [];
+  // List<int> acima = [] significa que voce está criando uma lista que vai conter apenas numeros inteiros
+  // também daria para criar a lista assim: "int acima[];", é so uma forma diferente de fazer.
   for (var numero in lista) {
     if (numero > media) {
       acima.add(numero);
